716ad7d55fb44cfd2431c2e57acd62027aea755c
Malcolm Wigren <trekommafem2@gmail.com>
2017-04-11 16:38:54 +0200

    Did some stuff

--- python/main.py
+++ python/main.py
@@ -1,25 +1,31 @@
-from scipy.fftpack import fft
+from scipy.fftpack import fft, ifft
 from scipy.io import wavfile
 from scipy import signal
 import matplotlib.pyplot as plt
 import numpy as np
+import math
+
 
 RAW_FILE = 'signal-malvi108.wav'
 
 
-def low_pass(data, order, cutoff):
-    b, a = signal.butter(order, cutoff, 'lowpass')
+def data_filter(data, order, cutoff, ftype):
+    b, a = signal.butter(order, cutoff, ftype, analog=True) 
     return signal.lfilter(b, a, data)
 
 
-def plot_filter(order, cutoff):
-    b, a = signal.butter(order, cutoff, 'lowpass', analog=True)
+def custom_filter(data, start, end):
+    transformed = fft(data)
+
+
+def plot_filter(order, cutoff, sample_rate):
+    b, a = signal.butter(order, cutoff, 'bandpass', analog=True)
     w, h = signal.freqs(b, a)
     plt.semilogx(w, abs(h))
     plt.title('Butterworth filter frequency response')
     plt.xlabel('Frequency [radians / second]')
     plt.grid(which='both', axis='both')
-    plt.axvline(cutoff, color='green') # cutoff frequency
+    # plt.axvline(cutoff, color='green') # cutoff frequency
     plt.show()
 
 
@@ -31,6 +37,8 @@ def plot_transform(data, sample_rate):
     num_samples = len(data)
     period = 1.0 / sample_rate
 
+    new_data = data_filter(data, 4, [0 * math.pi, 700000 * math.pi], 'bandpass')
+
     transformed = fft(data)
 
     xf = np.linspace(0.0, sample_rate // 2, num_samples // 2)
@@ -41,8 +49,8 @@ def plot_transform(data, sample_rate):
 
 def main():
     sample_rate, data = wavfile.read(RAW_FILE)
-    #plot_transform(data, sample_rate)
-    plot_filter(4, 1000)
+    plot_transform(data, sample_rate)
+    # plot_filter(4, [50000, 70000], sample_rate)
     
 
 if __name__ == "__main__":
